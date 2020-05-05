4235af3b11062569fe256e6633c8cb5b4c1a915b
Jakub Niemiec <janiemiec20@gmail.com>
2020-05-05 14:01:02 +0100

    Completed lesson 9

--- Quizzler-iOS13/Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Quizzler-iOS13/Model/QuizBrain.swift
@@ -25,9 +25,11 @@ struct QuizBrain {
     ]
     
     var questionNumber = 0
+    var score = 0
     
-    func checkAnswer(_ userAnswer: String) -> Bool {
+    mutating func checkAnswer(_ userAnswer: String) -> Bool {
         if userAnswer == quiz[questionNumber].answer {
+            score += 1
             return true
         } else {
             return false
@@ -47,7 +49,11 @@ struct QuizBrain {
             questionNumber += 1
         } else {
             questionNumber = 0
+            score = 0
         }
-        
+    }
+    
+    func getScore() -> Int {
+        return score
     }
 }
